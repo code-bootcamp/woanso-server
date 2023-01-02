c8dd3645c99316678b0e86ee32493af4f0154d65
HEisSHAM <114637197+HEisSHAM@users.noreply.github.com>
2023-01-03 02:42:32 +0900

    ✨ Feat : 만화 Search 기능 추가

--- backend/src/apis/comics/comics.module.ts
+++ backend/src/apis/comics/comics.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComicsResolver } from './comics.resolver';
 import { ComicsService } from './comics.service';
 import { Comic } from './entities/comic.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comic])],
+  imports: [
+    TypeOrmModule.forFeature([Comic]),
+    ElasticsearchModule.register({
+      node: 'http://elasticsearch:9200',
+    }),
+  ],
   providers: [ComicsService, ComicsResolver],
   exports: [ComicsService],
 })
