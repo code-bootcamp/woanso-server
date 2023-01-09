37a9ebbca0d413f97961a50ea2a06eef1de101c1
examplezi <cyei0824@gmail.com>
2023-01-09 18:08:07 +0900

    ✨Feat: 소셜로그인 기능 추가

--- backend/src/apis/adminAuth/admin.auth.service.ts
+++ backend/src/apis/adminAuth/admin.auth.service.ts
@@ -40,6 +40,9 @@ export class AdminAuthService {
       'Access-Control-Allow-Headers',
       'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
     );
+    //개발환경
+    // res.setHeader('Set-Cookie', `refreshToken=${refreshToken};path=/;`);
+    // 배포환경;
     res.setHeader(
       'Set-Cookie',
       `refreshToken=${refreshToken}; domain=.examplezi.shop; SameSite=None; Secure; httpOnly; path=/;`,
