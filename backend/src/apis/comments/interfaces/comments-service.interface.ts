8a1f4c8152d4bf2905715206994a0171652f5f5a
HEisSHAM <114637197+HEisSHAM@users.noreply.github.com>
2023-01-12 18:03:13 +0900

    🐛 Fix : Create comment 수정

--- backend/src/apis/comments/interfaces/comments-service.interface.ts
+++ backend/src/apis/comments/interfaces/comments-service.interface.ts
@@ -1,7 +1,9 @@
+import { IContext } from 'src/commons/types/context';
 import { CreateCommentInput } from '../dto/create-comment.input';
 
 export interface ICommentsServiceCreate {
   createCommentInput: CreateCommentInput;
+  context?: IContext;
 }
 
 export interface ICommentsServiceFindOne {
